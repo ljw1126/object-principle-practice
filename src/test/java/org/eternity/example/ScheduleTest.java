7086507f401a7ec4ebe3a1146def37428e589923
이진우 <leejinwoo1126@gmail.com>
2026-03-28 20:42:49 +0900

    test: Schedule 관련 테스트 리팩터링

--- src/test/java/org/eternity/example/ScheduleTest.java
+++ src/test/java/org/eternity/example/ScheduleTest.java
@@ -5,18 +5,29 @@ import java.time.DayOfWeek;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 public class ScheduleTest {
+    
     @Test
-    public void includes() {
+    public void day_of_week_in_month_includes() {
         Schedule schedule = new Schedule(
       "미팅",
              LocalTime.of(13, 0), 
              Duration.ofHours(1), 
-             2, 
-             DayOfWeek.MONDAY
+             new MonthlyPlan(2, DayOfWeek.MONDAY)
         );
+
+        assertThat(schedule.includes(LocalDate.of(2024, 7, 8))).isTrue();
+    }
+
+    @Test
+    public void day_of_weeks_includes() {
+        Schedule schedule = new Schedule(
+                "미팅", LocalTime.of(13, 0), Duration.ofHours(1),
+                new WeeklyPlan(Set.of(DayOfWeek.MONDAY, DayOfWeek.TUESDAY)));
+
         assertThat(schedule.includes(LocalDate.of(2024, 7, 8))).isTrue();
     }
 }
