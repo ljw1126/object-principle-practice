c045a7cdeb31cfba565939dc7c179a7f3bf46274
이진우 <leejinwoo1126@gmail.com>
2026-03-30 22:05:10 +0900

    refactor: PlayerTest에 try-catch 제거 > assertThatThrownBy 적용

--- src/test/java/org/eternity/adventure/PlayerTest.java
+++ src/test/java/org/eternity/adventure/PlayerTest.java
@@ -1,7 +1,7 @@
 package org.eternity.adventure;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.fail;
+import org.assertj.core.api.Assertions;
 import org.eternity.adventure.constant.Direction;
 import org.eternity.adventure.vo.Position;
 import org.eternity.adventure.vo.Size;
@@ -25,7 +25,6 @@ public class PlayerTest {
 
     @Test
     public void can_not_move_west() {
-        try {
             WorldMap worldMap = new WorldMap(
                     Size.with(2, 2),
                     new Room(Position.of(0, 0), "", ""),
@@ -34,14 +33,12 @@ public class PlayerTest {
             Player player = new Player(worldMap, Position.of(1, 1));
 
             assertThat(player.canMove(Direction.WEST)).isFalse();
-            player.move(Direction.WEST);
-            fail();
-        } catch (IllegalArgumentException ex) {}
+            Assertions.assertThatThrownBy(() -> player.move(Direction.WEST))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void can_not_move_outside() {
-        try {
             WorldMap worldMap = new WorldMap(
                     Size.with(2, 2),
                     new Room(Position.of(0, 0), "", ""),
@@ -50,8 +47,7 @@ public class PlayerTest {
             Player player = new Player(worldMap, Position.of(1, 1));
 
             assertThat(player.canMove(Direction.EAST)).isFalse(); // (2,1) Room 없음
-            player.move(Direction.EAST);
-            fail();
-        }catch (IllegalArgumentException ex) {}
+            Assertions.assertThatThrownBy(() -> player.move(Direction.EAST))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
