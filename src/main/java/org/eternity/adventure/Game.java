2a516143eefaac38b7dfa9467aedfce6d19c1588
이진우 <leejinwoo1126@gmail.com>
2026-04-02 08:17:52 +0900

    refactor: Game내 숨겨진 의존성을 외부에서 주입 받도록 변경

--- src/main/java/org/eternity/adventure/Game.java
+++ src/main/java/org/eternity/adventure/Game.java
@@ -2,8 +2,6 @@ package org.eternity.adventure;
 
 import java.util.Scanner;
 import org.eternity.adventure.constant.Direction;
-import org.eternity.adventure.vo.Position;
-import org.eternity.adventure.vo.Size;
 
 public class Game {
     private Player player;
@@ -11,18 +9,9 @@ public class Game {
     private boolean running;
     private InputOutput io;
 
-    public Game(InputOutput io) {
-        this.player = new Player(
-            new WorldMap(
-                Size.with(2, 3), 
-                new Room(Position.of(0, 0), "샘", "아름다운 샘물이 흐르는 곳입니다. 이곳에서 휴식을 취할 수 있습니다."),
-                new Room(Position.of(0, 1), "다리", "큰 강 위에 돌로 만든 커다란 다리가 있습니다."),
-                new Room(Position.of(1, 1), "성", "용왕이 살고 있는 성에 도착했습니다."),
-                new Room(Position.of(0, 2), "언덕", "저 멀리 성이 보이고 언덕 아래로 좁은 길이 나 있습니다."),
-                new Room(Position.of(1, 2), "동굴", "어둠에 잠긴 동굴 안에 작은 화톳불이 피어 있습니다.")
-            ), 
-            Position.of(0, 2));
-        this.commandParser = new CommandParser();
+    public Game(Player player, CommandParser commandParser, InputOutput io) {
+        this.player = player;
+        this.commandParser = commandParser;
         this.io = io;
     }
 
