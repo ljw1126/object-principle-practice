55fc751c695196df6b8350552a35de148a50bbf0
이진우 <leejinwoo1126@gmail.com>
2026-04-05 22:43:12 +0900

    test: Transfer 테스트 생성

---
+++ src/test/java/org/eternity/adventure/TransferTest.java
@@ -0,0 +1,31 @@
+package org.eternity.adventure;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.eternity.adventure.item.Inventory;
+import org.eternity.adventure.item.Item;
+import org.eternity.adventure.vo.Position;
+import org.junit.jupiter.api.Test;
+
+public class TransferTest {
+    @Test
+    public void transfer_is_possible() {
+        Room source = new Room(Position.of(0,0), "", "", new Inventory(new Item("item")));
+        Room target = new Room(Position.of(0,0), "", "", new Inventory(new Item("item")));
+
+        Transfer transfer = new Transfer(source, target, "item");
+
+        assertThat(transfer.isPossible()).isTrue();
+    }
+
+    @Test
+    public void transfer() {
+        Room source = new Room(Position.of(0,0), "", "", new Inventory(new Item("item")));
+        Room target = new Room(Position.of(0,0), "", "", new Inventory(new Item("item")));
+
+        Transfer transfer = new Transfer(source, target, "item");
+        transfer.perform();
+
+        assertThat(source.find("item")).isEmpty();
+        assertThat(target.find("item")).isPresent();
+    }
+}
